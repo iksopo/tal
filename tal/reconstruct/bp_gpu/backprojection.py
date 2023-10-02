802a0896ff6498f87b8eb76d571af2e582829c94
Óscar Pueyo Ciutad <oscar@graus.biz>
2023-10-02 17:00:55 +0200

    GPU brackprojection allows for parallel launching tasks

--- tal/reconstruct/bp_gpu/backprojection.py
+++ tal/reconstruct/bp_gpu/backprojection.py
@@ -1,4 +1,10 @@
 import numpy as np
+import gc
+
+from tal.config import get_memory_usage, get_resources
+from tqdm import tqdm
+import concurrent.futures
+import multiprocessing as mp
 
 def backproject(H_0, laser_grid_xyz, sensor_grid_xyz, volume_xyz,
 				camera_system, t_accounts_first_and_last_bounces,
@@ -33,6 +39,7 @@ def backproject(H_0, laser_grid_xyz, sensor_grid_xyz, volume_xyz,
 
 	reconstruction = __backproject_in_torch(H_0, x_l, sensor_grid_xyz, volume_xyz,
 						   d_14s, delta_t, nt, nv)
+	gc.collect()
 	torch.cuda.empty_cache()
 	return reconstruction
 
@@ -47,34 +54,40 @@ def __backproject_in_torch(H_0, x_l, sensor_grid_xyz, volume_xyz,
 	delta_t_torch = torch.tensor(delta_t).cuda()
 	nt_torch = torch.tensor(nt).cuda()
 	nv_torch = torch.tensor(nv).cuda()
-	
-	# In our case, jit doesn't improve the performance, just using GPU is enough
-	# It increases about 1.5 speedup if the method is called several times
-	# traced_bp = torch.jit.script(__backproject,
-	#						(H_0_torch, x_l_torch, sensor_grid_xyz_torch,
-	#							volume_xyz_torch, d_14s_torch, delta_t_torch,
-	#							nt_torch, nv_torch)
-	#						)
-	# reconstruction = traced_bp(H_0_torch, x_l_torch, sensor_grid_xyz_torch,
-	#							volume_xyz_torch, d_14s_torch, delta_t_torch,
-	#							nt_torch, nv_torch)
 
-	reconstruction = __backproject(H_0_torch, x_l_torch, sensor_grid_xyz_torch,
+	reconstruction = __backproject_parallel(H_0_torch, x_l_torch, sensor_grid_xyz_torch,
 			volume_xyz_torch, d_14s_torch, delta_t_torch,
 			nt_torch, nv_torch)
 	return reconstruction.cpu().numpy()
 
-def __backproject(H_0, x_l, sensor_grid_xyz, volume_xyz,
+def __backproject_parallel(H_0, x_l, sensor_grid_xyz, volume_xyz,
 					d_14s, delta_t, nt, nv):
 	H_1 = torch.zeros(nv, dtype=H_0.dtype, device='cuda')
-	v_range = torch.arange(0, volume_xyz.size(dim=0), dtype=torch.long, device='cuda')
 	s_range = torch.arange(0, sensor_grid_xyz.size(dim=0), dtype=torch.long, device='cuda')
-	for i_v in v_range:
-		x_v = volume_xyz[i_v]
-		x_s = sensor_grid_xyz
-		d_2 = torch.norm(x_l - x_v)
-		d_3 = torch.norm(x_v[None,:] - x_s, dim=1)
-		t_i = ((d_2 + d_3 + d_14s) / delta_t).long()
-		mask = torch.logical_and(t_i >= 0, t_i < nt)
-		H_1[i_v] = torch.sum(H_0[t_i[mask], s_range[mask]])
+
+	def work(subrange_v):
+		for i_v in subrange_v:
+			x_v = volume_xyz[i_v]
+			x_s = sensor_grid_xyz
+			d_2 = torch.norm(x_l - x_v)
+			d_3 = torch.norm(x_v[None,:] - x_s, dim=1)
+			t_i = ((d_2 + d_3 + d_14s) / delta_t).long()
+			mask = torch.logical_and(t_i >= 0, t_i < nt)
+			H_1[i_v] = torch.sum(H_0[t_i[mask], s_range[mask]])
+
+	max_cpu_cores = get_resources().cpu_processes
+	max_cpu_cores = mp.cpu_count() if max_cpu_cores == 'max' else max_cpu_cores
+	tasks_per_cpu_core = nv // max_cpu_cores
+	remaining_tasks = nv % max_cpu_cores
+	with concurrent.futures.ThreadPoolExecutor(max_workers=max_cpu_cores) as executor:
+		tasks = {executor.submit(work, (tasks_per_cpu_core+1)) : f'i' for i in range(max_cpu_cores)}
+		for i in range(max_cpu_cores):
+			ntasks = tasks_per_cpu_core if i >= remaining_tasks else (tasks_per_cpu_core+1)
+			start = i * tasks_per_cpu_core + min(remaining_tasks, i)
+			subrange_v = range(start, start + ntasks)
+			tasks[executor.submit(work, subrange_v)] = f'{i}'
+		with tqdm(total=max_cpu_cores) as pbar:
+			for future in concurrent.futures.as_completed(tasks):
+				name = tasks[future]
+				pbar.update()
 	return H_1
